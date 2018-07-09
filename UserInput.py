1b3e4a344b8384c8925981f41161064470b96e6c
linna-qian <linnaqian776@gmail.com>
2018-07-09 09:04:30 -0600

    Displaying the data

--- UserInput.py
+++ UserInput.py
@@ -1,4 +1,25 @@
 from textblob import TextBlob
-user_input = input()
-sentence = TextBlob(user_input)
-print(sentence.sentiment)
+import urllib.request, json
+import math
+import itertools
+
+with urllib.request.urlopen("https://api.github.com/repos/apache/hadoop/comments") as url:
+    data = json.loads(url.read().decode()) 
+    sequenceDict = {}
+    for index, elem in enumerate(data):
+        sequenceDict[index] = elem
+        if 'created_at' in elem:
+            dates = []
+            dates = elem['created_at'] 
+            shortdates = dates[:10]
+            if "html lang" in elem['body']:
+                print("This body contains additional html text.")
+            else:
+                messages = []
+                messages = elem['body']
+                sentence = TextBlob(elem['body'])
+                sentiments = []
+                sentiments = sentence.sentiment.polarity
+                shortsenti = '%.2f' % sentiments
+                tabledata = [shortdates, messages, shortsenti]
+                print(tabledata)
