3cb1952e643b6768c177b7d124a814bc14d782eb
linna-qian <linnaqian776@gmail.com>
2018-07-13 11:13:23 -0600

    Printing date, hash, sentiment, and build status

--- buildstatus.py
+++ buildstatus.py
@@ -1,24 +1,43 @@
 import urllib.request, json
+from textblob import TextBlob
 
 def print_information(inputrepo):
-    with urllib.request.urlopen("https://api.github.com/repos/mtedone/podam/statuses/f5ef93dd0aadc4ce67f06fa3cdc0dca1f703b8a4") as url:
+    with urllib.request.urlopen("https://api.github.com/repos/mtedone/podam/commits") as url:
         data = json.loads(url.read().decode()) 
         sequenceDict = {}
         for index, elem in enumerate(data):
             sequenceDict[index] = elem
-            if 'created_at' in elem:
-                dates = []
-                dates = elem['created_at'] 
-                shortdates = dates[:10]
-                if 'state' in elem:
-                    statuses = []
-                    statuses = elem['state']
-                    dayresult = [shortdates, statuses]
-                    print(dayresult)
+            if 'sha' in elem:
+                commithash = []
+                commithash = elem['sha']
+                if 'commit' in elem:
+                    commit = []
+                    commit = elem['commit']
+                    if 'message' in commit:
+                        message = []
+                        message = commit['message']
+                        sentence = TextBlob(message)
+                        sentiments = []
+                        sentiments = sentence.sentiment.polarity
+                        shortsenti = '%.2f' % sentiments
+                        statusurl = "https://api.github.com/repos/mtedone/podam/statuses" + "/" + commithash
+                        with urllib.request.urlopen(statusurl) as url2:
+                            data2 = json.loads(url2.read().decode())
+                            sequenceDict2 = {}
+                            for num, item in enumerate(data2):
+                                sequenceDict2[num] = item
+                                if 'created_at' in item:
+                                    dates = []
+                                    dates = item['created_at'] 
+                                    shortdates = dates[:10]
+                                    if 'state' in item:
+                                        statuses = []
+                                        statuses = item['state']
+                                        print([shortdates, commithash, shortsenti, statuses])
 
 print("What is the repository name?")
 inputrepo = input()
-if inputrepo == 'podam':
+if inputrepo == 'mtedone/podam':
     print_information(inputrepo)
 else:
     print("No repository information available.")
